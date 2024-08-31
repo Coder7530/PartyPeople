ad042778152f230944d0dd95c3bc52ab631458d4
Iliyan Dimitrov <iliyan.dimitrovpr@gmail.com>
2024-08-31 18:11:00 +0100

    Added EventEmployees SQL table and procedure scripts to enable adding employees to events.

---
+++ Database/api/Stored Procedures/spEventEmployeeList.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [api].[spEventEmployeeList]
+    @EventId INT
+AS
+BEGIN
+    SELECT e.*
+    FROM [dbo].[Employee] e
+    INNER JOIN [dbo].[EventEmployees] ee ON e.Id = ee.EmployeeId
+    WHERE ee.EventId = @EventId
+END
