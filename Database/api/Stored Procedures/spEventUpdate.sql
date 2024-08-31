53fc48775e8d5dd33fdc5449eba5bc6b09ffb9be
Iliyan Dimitrov <iliyan.dimitrovpr@gmail.com>
2024-08-31 16:50:19 +0100

    Added the missing event id to the event update procedure and event repo update method.

--- Database/api/Stored Procedures/spEventUpdate.sql
+++ Database/api/Stored Procedures/spEventUpdate.sql
@@ -1,6 +1,7 @@
 ﻿
 CREATE   PROC [api].[spEventUpdate]
 (
+    @Id INT,
     @Description [nvarchar](255),
     @StartDateTime [datetime2],
     @EndDateTime [datetime2],
@@ -8,15 +9,17 @@ CREATE   PROC [api].[spEventUpdate]
 )
 AS
 BEGIN
-    UPDATE  [E]
-    SET     [E].[Description] = @Description,
-            [E].[StartDateTime] = @StartDateTime,
-            [E].[EndDateTime] = @EndDateTime,
-            [E].[MaximumCapacity] = @MaximumCapacity
-    OUTPUT [Inserted].[Id],
-           [Inserted].[Description],
-           [Inserted].[StartDateTime],
-           [Inserted].[EndDateTime],
-           [Inserted].[MaximumCapacity]
-    FROM    [dbo].[Event] AS [E];
+    UPDATE [E]
+    SET [E].[Description] = @Description,
+        [E].[StartDateTime] = @StartDateTime,
+        [E].[EndDateTime] = @EndDateTime,
+        [E].[MaximumCapacity] = @MaximumCapacity
+    OUTPUT 
+        [Inserted].[Id],
+        [Inserted].[Description],
+        [Inserted].[StartDateTime],
+        [Inserted].[EndDateTime],
+        [Inserted].[MaximumCapacity]
+    FROM [dbo].[Event] AS [E]
+    WHERE [E].[Id] = @Id;
 END;
